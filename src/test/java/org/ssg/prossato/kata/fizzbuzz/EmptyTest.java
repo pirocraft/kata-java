a59b4e4cb3561a947be722fd15335420473346d2
prossato <pierre.rossato@soprasteria.com>
2020-06-04 16:25:14 +0200

    Init with java 8 junit 5 & assertJ. Empty test

---
+++ src/test/java/org/ssg/prossato/kata/fizzbuzz/EmptyTest.java
@@ -0,0 +1,12 @@
+package org.ssg.prossato.kata.fizzbuzz;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class EmptyTest {
+    @Test
+    public void emptyTest() {
+        fail("EmptyTest");
+    }
+}
