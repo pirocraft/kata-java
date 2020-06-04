969cbc0bb0487217b20a84d0551a3d9a7b5b3a69
prossato <pierre.rossato@soprasteria.com>
2020-06-04 16:39:17 +0200

    Refacto test

--- src/test/java/org/ssg/prossato/kata/fizzbuzz/FizzBuzzShould.java
+++ src/test/java/org/ssg/prossato/kata/fizzbuzz/FizzBuzzShould.java
@@ -1,22 +1,26 @@
 package org.ssg.prossato.kata.fizzbuzz;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FizzBuzzShould {
 
+    private FizzBuzz fizzBuzz;
+
+    @BeforeEach
+    void setUp() {
+        fizzBuzz = new FizzBuzz();
+    }
+
     @Test
     public void return_1_when_1() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
         assertThat(fizzBuzz.fizzBuzz(1)).isEqualTo(1);
     }
 
     @Test
     public void return_2_when_2() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
         assertThat(fizzBuzz.fizzBuzz(2)).isEqualTo(2);
     }
 }
