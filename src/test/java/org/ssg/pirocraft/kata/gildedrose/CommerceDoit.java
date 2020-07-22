4c77a2d18f049c92cd3662a2dd0dd3b35553e1a2
prossato <pierre.rossato@soprasteria.com>
2020-07-22 18:24:33 +0200

    Test avec un autre produit et renommage des produits

--- src/test/java/org/ssg/pirocraft/kata/gildedrose/CommerceDoit.java
+++ src/test/java/org/ssg/pirocraft/kata/gildedrose/CommerceDoit.java
@@ -12,22 +12,33 @@ import static org.assertj.core.api.Assertions.assertThat;
 class CommerceDoit {
     private ProduitRepository produitRepository;
     private Commerce commerce;
-    private UUID idProduit;
+
+    private static final int QUALITE_FIGURINE_BATMAN = 5;
+    private static final int QUALITE_TASER = 10;
+    private UUID idFigurineBatman;
+    private UUID idTaser;
 
     @BeforeEach
     void setUp() {
         produitRepository = new InMemoryProduitRepository();
         commerce = new Commerce(produitRepository);
-        Produit produit = new Produit();
-        produit.setQualite(5);
-        idProduit = produitRepository.addProduit(produit);
+
+        Produit figurineBatman = new Produit();
+        figurineBatman.setQualite(QUALITE_FIGURINE_BATMAN);
+        idFigurineBatman = produitRepository.addProduit(figurineBatman);
+
+        Produit taser = new Produit();
+        taser.setQualite(QUALITE_TASER);
+        idTaser = produitRepository.addProduit(taser);
     }
 
     @Test
     void mettre_a_jour_la_qualite_des_produits() {
         commerce.mettreAJour();
 
-        Produit produitStocke = produitRepository.getProduit(idProduit);
-        assertThat(produitStocke.getQualite()).isEqualTo(4);
+        Produit figurineBatmanStockee = produitRepository.getProduit(idFigurineBatman);
+        Produit taserStocke = produitRepository.getProduit(idTaser);
+        assertThat(figurineBatmanStockee.getQualite()).isEqualTo(QUALITE_FIGURINE_BATMAN - 1);
+        assertThat(taserStocke.getQualite()).isEqualTo(QUALITE_TASER - 1);
     }
 }
